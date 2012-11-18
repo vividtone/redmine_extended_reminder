7a27e6fe6cf9e667d52647fad9d7dc9d4e388a8f
MAEDA Go <maeda@farend.jp>
2012-11-18 23:05:08 +0900

    check 'no reminders' status when 'Send reminder now' is performed.

--- app/controllers/redmine_extended_reminder/settings_controller.rb
+++ app/controllers/redmine_extended_reminder/settings_controller.rb
@@ -12,8 +12,12 @@ class RedmineExtendedReminder::SettingsController < ApplicationController
 
   def send_reminder
     user = User.current
-    Mailer.reminders(:users => [user.id])
-    flash[:notice] = l(:notice_email_sent, :value => user.mail)
+    unless user.pref[:extended_reminder_no_reminders]
+      Mailer.reminders(:users => [user.id])
+      flash[:notice] = l(:notice_email_sent, :value => user.mail)
+    else
+      flash[:error] = l(:extended_reminder_no_reminders_is_set)
+    end
     redirect_to :controller => '/my', :action => 'account'
   end
 end
