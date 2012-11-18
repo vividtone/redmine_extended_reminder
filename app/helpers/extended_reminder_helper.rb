d32935f9c848839026af5900b98a772d93041b98
MAEDA Go <maeda@farend.jp>
2012-11-18 14:50:22 +0900

    helper for the plugin.

---
+++ app/helpers/extended_reminder_helper.rb
@@ -0,0 +1,18 @@
+module ExtendedReminderHelper
+  def due_date_in_words(due_date)
+    case due_date
+    when Date.yesterday
+      l(:label_yesterday)
+    when Date.today
+      l(:label_today)
+    when Date.tomorrow
+      l(:label_tomorrow)
+    else
+      if due_date < Date.today
+        l(:extended_reminder_days_ago, :days => distance_of_time_in_words_to_now(due_date))
+      else
+        l(:extended_reminder_days_after, :days => distance_of_time_in_words_to_now(due_date))
+      end
+    end
+  end
+end
