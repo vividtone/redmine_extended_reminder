6103ccefcdecea177adb720638f6214763e06d12
MAEDA Go <maeda@farend.jp>
2012-11-20 09:10:44 +0900

    fix: wrong value of "xx days (ago|after)".

--- app/helpers/extended_reminder_helper.rb
+++ app/helpers/extended_reminder_helper.rb
@@ -1,17 +1,19 @@
 module ExtendedReminderHelper
   def due_date_in_words(due_date)
+    today = Date.today
     case due_date
     when Date.yesterday
       l(:label_yesterday)
-    when Date.today
+    when today
       l(:label_today)
     when Date.tomorrow
       l(:extended_reminder_tomorrow)
     else
-      if due_date < Date.today
-        l(:extended_reminder_days_ago, :days => distance_of_time_in_words_to_now(due_date))
+      days_in_word = distance_of_time_in_words(today, due_date)
+      if due_date < today
+        l(:extended_reminder_days_ago, :days => days_in_word)
       else
-        l(:extended_reminder_days_after, :days => distance_of_time_in_words_to_now(due_date))
+        l(:extended_reminder_days_after, :days => days_in_word)
       end
     end
   end
