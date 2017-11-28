34db7781eae21e397945dc201a537d8bb924338b
MAEDA Go <maeda@farend.jp>
2017-11-28 08:43:12 +0000

    Fix for compatibility with Redmine 3.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 namespace :redmine_extended_reminder do
-  match 'settings/update' => 'settings#update'
-  match 'settings/send_reminder' => 'settings#send_reminder'
+  match 'settings/update' => 'settings#update', :via => [:post]
+  match 'settings/send_reminder' => 'settings#send_reminder', :via => [:get, :post]
 end
